81ba0a77ca646c28ad62e67b2e668767d9d9deb9
“[Mateo <[lmateo@uninorte.edu.co]>
2018-08-23 17:34:24 -0500

    se añade a '2' la habilidad de seleccionar, cual de los nodos sin color conectados con nodos con color (colors matrix)  es el que tiene el mayor numero de conecciones con colores distintos

--- busqueda.py
+++ busqueda.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+# !!! RECORDAR QUE CONTEO DE MATRICES EMPIEZA EN 0
 """
 Spyder Editor
 
@@ -7,10 +9,17 @@ This is a temporary script file.
 import numpy as np 
 
 def search(entry):
+    """ Funcion de busqueda, con entrada de matrix de adyacencia siempre actual
+    y con salida para funcion de Haroldo: nodo con mayor numero de colores 
+    diferentes conectados a el "heuristica" (af), lista de dichos colores 
+    (final_list) y el nodo de interes cuyas propiedades se listaron 
+    ariba (bf)"""
 
-#//////////////////////////////////////////////////////////////////////////////
+#////////////////////////////////////ONE///////////////////////////////////////
 #Busco las diagonales con cero (eso indica sin color asignado) y retorno que 
-#filas de la matrix de adyacencia son aquellas sin color asignado -> "noColor"   
+#filas de la matrix de adyacencia son aquellas sin color asignado -> "noColor" 
+    print(entry)
+    print("*")
     [i,j] = np.where(entry==0)
     noColor = []
     for k in range(i.shape[0]) :
@@ -18,30 +27,45 @@ def search(entry):
             noColor = np.append(noColor,[i[k]])
     noColor = noColor.astype(int)
     print(noColor)
+    print("*")
 #//////////////////////////////////////////////////////////////////////////////
     
     
-#//////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////TWO////////////////////////////////////////
 #Me indica con que colores estan conectados aquellos puntos sin color asignado,
 #retorno una matrix de colores donde cada columna de esta corresponde a una
 #fila de la matriz noColor. La matrix colores posee en cada elemnto de una fila 
 #los colores con los que el nodo esta conectado -> "colores"
     colors = np.zeros((noColor.shape[0], entry.shape[0]))
+    af = -1
     for k1 in range(noColor.shape[0]):
         for k2 in range(entry.shape[0]):
             if (entry[noColor[k1],k2] == 1):
                 colors[k1,k2] = entry[k2,k2]
+        med_list = []
+        for num in colors[k1,:]:
+            if (num not in med_list) and (num != 0) :
+                med_list.append(num)
+        a = len(med_list)
+        b = noColor[k1]
+        if a > af:
+            af = a
+            bf = b
+            final_list = list(map(int, med_list))
     print(colors)
-#//////////////////////////////////////////////////////////////////////////////
-
-
-#//////////////////////////////////////////////////////////////////////////////
-
+    print("*")
+    print(final_list) #Color list 
+    print("*")
+    print(af) #Heuristic
+    print("*")
+    print(bf) #Node
+    
+    return (final_list,af,bf)
 #//////////////////////////////////////////////////////////////////////////////    
     
-entry = np.array([[1,1,1,1,0,0],[1,2,0,0,0,0],[1,0,0,0,0,1],[1,0,0,0,1,1],
-              [0,0,0,1,0,0],[0,0,1,1,1,0]]) #matrientry de entada 
-print(entry)
+entry = np.array([[1,1,1,1,0,0],[1,2,1,0,1,0],[1,0,0,1,1,1],[1,1,0,3,1,1],
+              [1,0,1,1,0,1],[1,1,0,1,1,0]]) #matrientry de entada 
+    
 search(entry)   
     
     
